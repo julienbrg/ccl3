2edb4ce0da3bee0f61c6509a379ae84a4c1c98f8
julienbrg <julien@strat.cc>
2021-11-27 19:48:20 +0100

    fix name

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -9,18 +9,23 @@ import useWeb3Modal from "./hooks/useWeb3Modal";
 import { addresses, abis } from "@project/contracts";
 import GET_TRANSFERS from "./graphql/subgraph";
 
-async function readOnChainData() {
+async function readOnChainData(account) {
+
+  console.log("user1: ", account);
+
   // Should replace with the end-user wallet, e.g. Metamask
   const defaultProvider = getDefaultProvider();
   // Create an instance of an ethers.js Contract
   // Read more about ethers.js on https://docs.ethers.io/v5/api/contract/contract/
   const ceaErc20 = new Contract(addresses.ceaErc20, abis.erc20, defaultProvider);
+
   // A pre-defined address that owns some CEAERC20 tokens
-  const tokenBalance = await ceaErc20.balanceOf("0x3f8CB69d9c0ED01923F11c829BaE4D9a4CB6c82C");
+  const tokenBalance = await ceaErc20.balanceOf(account);
   console.log({ tokenBalance: tokenBalance.toString() });
 }
 
 function WalletButton({ provider, loadWeb3Modal, logoutOfWeb3Modal }) {
+
   const [account, setAccount] = useState("");
   const [rendered, setRendered] = useState("");
 
@@ -72,6 +77,26 @@ function WalletButton({ provider, loadWeb3Modal, logoutOfWeb3Modal }) {
 function App() {
   const { loading, error, data } = useQuery(GET_TRANSFERS);
   const [provider, loadWeb3Modal, logoutOfWeb3Modal] = useWeb3Modal();
+  const [account, setAccount] = useState("");
+
+  useEffect(() => {
+    async function fetchAccount() {
+      try {
+        if (!provider) {
+          return;
+        }
+
+        // Load the user's accounts.
+        const accounts = await provider.listAccounts();
+        setAccount(accounts[0]);
+        
+      } catch (err) {
+        setAccount("");
+        console.error(err);
+      }
+    }
+    fetchAccount();
+  }, [account, provider, setAccount]);
 
   React.useEffect(() => {
     if (!loading && !error && data && data.transfers) {
@@ -86,11 +111,11 @@ function App() {
       </Header>
       <Body>
         <p>
-          Hello! 
+           {account}
         </p>
         {/* Remove the "hidden" prop and open the JavaScript console in the browser to see what this function does */}
-        <Button onClick={() => readOnChainData()}>
-          Read On-Chain Balance
+        <Button onClick={() => readOnChainData(account)}>
+          Check my balance
         </Button>
         <Link href="" style={{ marginTop: "8px" }}></Link>
       </Body>
